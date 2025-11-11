f85c9b320263e0508f61c054ed341f7f6a7a15af
Bruno Rodrigues <rodriguesbruno@hotmail.com>
2025-11-11 14:22:54 +0000

    ci

--- build.sh
+++ build.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-set -e
+set -ex
 
 MICROPYTHON_DIR="$PWD/micropython"
 PICOBRIDGE_DIR="$PWD"
 
-# Clone MicroPython if not already present
 if [ ! -d "$MICROPYTHON_DIR" ]; then
-  echo "📥 Cloning MicroPython..."
   git clone --recursive https://github.com/micropython/micropython.git "$MICROPYTHON_DIR"
 fi
 
-# Get version
+cd "$MICROPYTHON_DIR/mpy-cross"
+make -j$(nproc)
+
 VERSION=$(grep '__version__' "$PICOBRIDGE_DIR/src/pb_version.py" | cut -d'"' -f2)
-echo "📦 Building PicoBridge version $VERSION"
+echo "📦 Building PicoBridge $VERSION"
 
 cd "$MICROPYTHON_DIR/ports/rp2"
 rm -rf build-RPI_PICO2_W
 
-make V=1 BOARD=RPI_PICO2_W \
+make BOARD=RPI_PICO2_W \
      FROZEN_MANIFEST=$PICOBRIDGE_DIR/my_manifest.py \
      -j$(nproc)
 
 cp build-RPI_PICO2_W/firmware.uf2 "$PICOBRIDGE_DIR/picobridge_${VERSION}.uf2"
-echo "✅ Done: $PICOBRIDGE_DIR/picobridge_${VERSION}.uf2"
