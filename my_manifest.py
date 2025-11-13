16d50df78fa85de0e6fc1d954a6d09f72eaad173
Bruno Rodrigues <rodriguesbruno@hotmail.com>
2025-11-13 14:02:22 +0000

    CI

--- my_manifest.py
+++ my_manifest.py
@@ -1,3 +1,19 @@
 include("$(MPY_DIR)/ports/rp2/boards/manifest.py")
-freeze("src", opt=3)
+# Freeze all Python files in top-level project folder
+freeze(".", ("main.py",))
+
+# Freeze an entire folder of .py files
+freeze("src")
+
+# Freeze Microdot framework
+freeze("microdot")
+
+# Freeze utemplate (template engine)
+freeze("utemplate")
+
+# Freeze your templates (HTML files become frozen strings)
+freeze("templates")
+
+# Freeze static files (JS, CSS, etc.)
+freeze("static")
 
